bb1cd71d9c2f933c1e5d80e7494d68c45633bb6b
chancialejandra <99815344+chancialejandra@users.noreply.github.com>
2023-04-10 23:05:28 -0500

    Metodo agregado al controler

--- src/main/java/com/co/pds/User/controller/UsuarioController.java
+++ src/main/java/com/co/pds/User/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.co.pds.User.controller;
 
 import com.co.pds.User.dto.Request.UsuarioRequest;
 import com.co.pds.User.Service.IUsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @Validated
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    private final IUsuarioService iUsuarioService;
+
+    @PostMapping()
+    public ResponseEntity crearUsuario(@Valid @RequestBody UsuarioRequest usuario){
+        var response = iUsuarioService.crearUsuario(usuario);
+        return ResponseEntity.status(response.status).body(response);
+    }
 
 }
